1cb7021ae6410b21076b9bc763c47696aecf6c38
Andre Ximenes <andreximenes536@gmail.com>
2026-05-08 15:35:37 -0300

    feat(api): enable Helmet and CORS in bootstrap

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter'
 import { setupDocs } from './infra/docs/setup-docs'
@@ -8,6 +9,21 @@ import { EnvService } from './infra/env/env.service'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const env = app.get(EnvService)
+
+  app.use(
+    env.envMode === 'PROD'
+      ? helmet()
+      : helmet({
+          contentSecurityPolicy: false,
+        }),
+  )
+
+  const corsOrigins = env.corsOrigins
+  if (corsOrigins === true) {
+    app.enableCors({ origin: true, credentials: true })
+  } else if (corsOrigins.length > 0) {
+    app.enableCors({ origin: corsOrigins, credentials: true })
+  }
 
   app.useGlobalPipes(
     new ValidationPipe({
